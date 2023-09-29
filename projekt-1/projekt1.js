0104069a3370c838ad1342ab8c8f614af15d5929
b0totdav@vasvari.org <b0totdav@vasvari.org>
2023-09-29 09:40:29 +0200

    1.2.1

---
+++ projekt-1/projekt1.js
@@ -0,0 +1,9 @@
+let Eclipse=document.getElementById(lightAndDark)
+
+Eclipse.onclick()
+{
+    if(Eclipse.=="Sötét mód"){
+        Eclipse.innerText="Világos mód";
+        //document.getElementsByTagName("body").style.background-image=url('../kepek/2.png');
+    }
+}
