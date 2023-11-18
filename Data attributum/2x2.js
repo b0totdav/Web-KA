b88a8a0e3745abdc83add2ad3493c98f521d7430
b0totdav <b0totdav@vasvari.org>
2023-11-18 14:05:33 +0100

    Kesz program

--- Data attributum/2x2.js
+++ Data attributum/2x2.js
@@ -1,3 +1,21 @@
-let tomb=document.getElementsByClassName("item");
+let container=document.getElementById("container");
+let colors=["yellow", "yellow", "yellow", "yellow"];
 
-function
+
+function Colorchange(index){
+    if(colors[index]=="Red"){
+        colors[index]="Blue";
+    }
+    else{
+        colors[index]="Red";
+    }
+    Rajzolas();
+}
+
+function Rajzolas(){
+    let ki="";
+    for (let index = 0; index < 4; index++) {
+        ki+='<div class="item" onclick="Colorchange('+index+')" style="background-color:'+colors[index]+';"></div>'
+    }
+    container.innerHTML=ki;
+}
