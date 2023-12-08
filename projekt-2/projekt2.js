a82ed4ee7098b02540e4a682ab838dab0c22950d
b0totdav@vasvari.org <b0totdav@vasvari.org>
2023-12-08 11:29:23 +0100

    1.4

--- projekt-2/projekt2.js
+++ projekt-2/projekt2.js
@@ -1,25 +1,51 @@
 let keret=document.getElementById("container");
 
-const boss = new Image();
-boss.src = "../kepek/projekt2/Bosskicsi.png";
-keret.appendChild(boss);
 
-const fixer = new Image();
-fixer.src = "../kepek/projekt2/Fixerkicsi.png";
-keret.appendChild(fixer);
+function setBoss()
+{
+    const boss = new Image();
+    boss.src = "../kepek/projekt2/Bosskicsi.png";
+    keret.appendChild(boss);
+}
 
-const gregor = new Image();
-gregor.src = "../kepek/projekt2/Gregorkicsi.png";
-keret.appendChild(gregor);
+function setFixer()
+{
+    const fixer = new Image();
+    fixer.src = "../kepek/projekt2/Fixerkicsi.png";
+    keret.appendChild(fixer);
+}
 
-const rex = new Image();
-rex.src = "../kepek/projekt2/rexkicsi.png";
-keret.appendChild(rex);
+function setGregor()
+{
+    const gregor = new Image();
+    gregor.src = "../kepek/projekt2/Gregorkicsi.png";
+    keret.appendChild(gregor);
+}
 
-const scorch = new Image();
-scorch.src = "../kepek/projekt2/Scorchkicsi.png";
-keret.appendChild(scorch);
+function setRex()
+{
+    const rex = new Image();
+    rex.src = "../kepek/projekt2/rexkicsi.png";
+    keret.appendChild(rex);
+}
 
-const sev = new Image();
-sev.src = "../kepek/projekt2/Sevkicsi.png";
-keret.appendChild(sev);
+function setScorch()
+{
+    const scorch = new Image();
+    scorch.src = "../kepek/projekt2/Scorchkicsi.png";
+    keret.appendChild(scorch);
+}
+
+function setSev()
+{
+    const sev = new Image();
+    sev.src = "../kepek/projekt2/Sevkicsi.png";
+    keret.appendChild(sev);
+}
+
+
+let lista={"SetBoss":setBoss(),"SetBoss":setBoss(),"Setsev":setSev(),"SetSev":setSev(),"SetFixer":setFixer(),"SetFixer":setFixer(),"SetScorch":setScorch(),"SetScorch":setScorch(),"SetRex":setRex(),"SetRex":setRex(),"SetGregor":setGregor(),"SetGregor":setGregor()}
+
+for (let index in lista) {
+    lista[index];
+}
