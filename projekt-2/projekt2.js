50104b1a547a122bd900276d87f04b3498a33604
b0totdav <b0totdav@vasvari.org>
2023-12-13 18:24:34 +0100

    test 1

--- projekt-2/projekt2.js
+++ projekt-2/projekt2.js
@@ -5,6 +5,7 @@ function setBoss()
 {
     const boss = new Image();
     boss.src = "../kepek/projekt2/Bosskicsi.png";
+    boss.setAttribute("class","elso")
     keret.appendChild(boss);
 }
 
@@ -12,6 +13,7 @@ function setFixer()
 {
     const fixer = new Image();
     fixer.src = "../kepek/projekt2/Fixerkicsi.png";
+    fixer.setAttribute("class","masodik")
     keret.appendChild(fixer);
 }
 
@@ -19,6 +21,7 @@ function setGregor()
 {
     const gregor = new Image();
     gregor.src = "../kepek/projekt2/Gregorkicsi.png";
+    gregor.setAttribute("class","harmadik")
     keret.appendChild(gregor);
 }
 
@@ -26,6 +29,7 @@ function setRex()
 {
     const rex = new Image();
     rex.src = "../kepek/projekt2/rexkicsi.png";
+    rex.setAttribute("class","negyedik")
     keret.appendChild(rex);
 }
 
@@ -33,6 +37,7 @@ function setScorch()
 {
     const scorch = new Image();
     scorch.src = "../kepek/projekt2/Scorchkicsi.png";
+    scorch.setAttribute("class","otodik")
     keret.appendChild(scorch);
 }
 
@@ -40,13 +45,31 @@ function setSev()
 {
     const sev = new Image();
     sev.src = "../kepek/projekt2/Sevkicsi.png";
+    sev.setAttribute("class","hatodik")
     keret.appendChild(sev);
 }
 
 
 let lista={"SetBoss":setBoss(),"SetBoss":setBoss(),"Setsev":setSev(),"SetSev":setSev(),"SetFixer":setFixer(),"SetFixer":setFixer(),"SetScorch":setScorch(),"SetScorch":setScorch(),"SetRex":setRex(),"SetRex":setRex(),"SetGregor":setGregor(),"SetGregor":setGregor()}
-let random={};
+let randomlista=["","","","","","","","","","","",""];
+let lenght=lista.lenght;
 
-for (let index in lista) {
-    lista[index];
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+  
+for(var i=0;i<lenght;i++)
+{
+    let random=getRandomInt(12);
+    randomlista[i]=lista[random];
+    lista.push(random);
+}
+
+for (let index in randomlista) {
+    randomlista[index];
+}
+
+function sideSwitch(background)
+{
+    background.style.visibility="visible";
 }
