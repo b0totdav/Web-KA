718b5b132353422b1a6dc4d721b977c96a284bdf
b0totdav@vasvari.org <b0totdav@vasvari.org>
2024-01-12 09:36:53 +0100

    1.1

---
+++ Arrow fuggveny/arrow.js
@@ -0,0 +1,17 @@
+function Reverse()
+{
+    let e=document.getElementById("ki");
+    e.innerHTML="";
+    let szoveg =document.getElementById("szoveg").value;
+    console.log(szoveg);
+    const vissz=szoveg.split("");
+    console.log(vissz)
+    vissz.reverse();
+    console.log(vissz)
+    for(let betu in vissz)
+    {
+        e.innerHTML+=vissz[betu];
+    }
+}
+
+const Megforditas=()=>Reverse();
