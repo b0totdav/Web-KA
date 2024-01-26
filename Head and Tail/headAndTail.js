de4fb7bbc05e01c290c5c2134cd3b0a0cee818fc
b0totdav@vasvari.org <b0totdav@vasvari.org>
2024-01-26 11:24:35 +0100

    1.2

--- Head and Tail/headAndTail.js
+++ Head and Tail/headAndTail.js
@@ -35,4 +35,6 @@ function swapHeadAndTail(arr) {
     }
   }
 
-  console.log(tomb)
+  console.log(swapHeadAndTail([ 1, 2, 3, 4, 5 ] ), [ 4, 5, 3, 1, 2 ]);
+  console.log(swapHeadAndTail([ -1, 2 ]), [ 2, -1 ]);
+  console.log(swapHeadAndTail([ 1, 2, -3, 4, 5, 6, -7, 8 ]), [ 5, 6, -7, 8, 1, 2, -3, 4 ]);
