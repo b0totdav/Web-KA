91cccbb56c84c61a3d30df9bdd0cb6732a796f31
b0totdav@vasvari.org <b0totdav@vasvari.org>
2024-02-02 09:15:34 +0100

    kesz

---
+++ MeanVsMedian/meanVsMedian.js
@@ -0,0 +1,20 @@
+function meanVsMedian(numbers) {
+    let median = numbers[Math.floor(numbers.length/2)]
+      let sum=0;
+      for(let i=0; i<numbers.length; i++)
+      {
+        sum+=numbers[i]
+      }
+      let avg=sum/numbers.length
+      avg=Math.floor(avg)
+      
+      if(median<avg)
+      {
+        return "mean"
+      }else if(median>avg)
+      {
+        return "median"  
+      }else{
+        return "same"
+      }
+    }
