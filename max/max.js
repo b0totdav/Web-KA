683039b732f3960d7be34981a71a56faff263b42
b0totdav@vasvari.org <b0totdav@vasvari.org>
2024-02-02 10:57:35 +0100

    kesz

--- max/max.js
+++ max/max.js
@@ -1,23 +1,23 @@
 function adjacentElementsProduct(array) {
-    let szorzat+=0;
-    let temp+=0;
-    for(let i=0;i<array.length;i++)
+  let szorzat=Number.NEGATIVE_INFINITY;
+  let temp=Number.NEGATIVE_INFINITY;
+  for(let i=0;i<array.length;i++)
+  {
+    try
     {
-      try
+      temp=array[i]*array[i-1];
+      if(temp>szorzat)
       {
-        temp=array[i]*array[i-1];
-        if(temp>szorzat)
-        {
-          szorzat=temp;
-          temp=0
-        }
-        temp=array[i]*array[i+1];
-        if(temp>szorzat)
-        {
-          szorzat=temp;
-          temp=0
-        }
-      }catch{}
-    }
-      return szorzat
+        szorzat=temp;
+        temp=0
+      }
+      temp=array[i]*array[i+1];
+      if(temp>szorzat)
+      {
+        szorzat=temp;
+        temp=0
+      }
+    }catch{}
   }
+    return szorzat
+}
