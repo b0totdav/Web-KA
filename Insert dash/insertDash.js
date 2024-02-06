7b977c6f9f1160b8fcd356e8d425542e98af8b66
b0totdav@vasvari.org <b0totdav@vasvari.org>
2024-02-06 12:23:35 +0100

    kesz

--- Insert dash/insertDash.js
+++ Insert dash/insertDash.js
@@ -1,15 +1,14 @@
 function insertDash(num) {
-    let tomb=num.toString().split("");
-    for(let i=0;i<num.length-1;i++)
-    {
-      if((1*tomb[i])%2!=0 && (1 * tomb[i+1])%2!=0){
-        tomb[i]= tomb[i]+"-";
+  let tomb=num.toString().split("");
+ 
+  for(let i=0; i < tomb.length - 1; i++) {
+    if (1*tomb[i] % 2 != 0 && 1*tomb[i + 1] % 2 != 0) {
+      tomb.splice(i, 1, tomb[i] + "-");
     }
-    console.log(tomb);
-    let text=tomb.join("")
-    return text
- }
+  }
+  return tomb.join("");
 }
+
 
 console.log((insertDash(454793) + '4547-9-3'))
 console.log((insertDash(123456) + '123456'))
