ef89f63cc06cdd09005f72ddd8106480a3183076
b0totdav <b0totdav@vasvari.org>
2024-05-01 12:40:12 +0200

    haladunk

--- projekt/p.js
+++ projekt/p.js
@@ -1,21 +1,21 @@
 const url = "https://vvri.pythonanywhere.com/api/courses";
+const surl = "https://vvri.pythonanywhere.com/api/students";
 const ki=document.getElementById("container");
 const courseName=document.getElementById("courseName");
+let courseId;
+const eredmeny=document.getElementById("eredmeny");
+const dki=document.getElementById("students");
 
 function fetchCourses()
 {
-    ki.innerHTML="";
+    ki.innerHTML="<th>Kurzus száma</th><th>Kurzus neve</th>";
     fetch(url)
     .then(response => response.json())
     .then(data => {
         if (data)
             data.forEach(element => {
                 console.log(element)
-                ki.innerHTML+='<div class="course">'+element.name+'\n';
-                element.students.forEach(student => {
-                    ki.innerHTML+='<div class="students" onclick="deleteStudent(student.id)">&nbsp &nbsp'+student.name+'</div>'
-                });
-                ki.innerHTML+='</div>'
+                ki.innerHTML+='<tr class="course"><td>'+element.id+'</td><td>'+element.name+'</td></tr>';
         })
     })
     .catch(error => console.log("Hiba történt: " + error))
@@ -39,25 +39,33 @@ function createCourse() {
     .catch(error => console.error('Error creating course:', error));
 }
 
-function deleteStudent(studentId){
-
-    fetch("https://vvri.pythonanywhere.com/api/students/${studentId}", {
-
-        method: "DELETE",
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        }
-})
-    .then(response => {
-    if (response.ok) {
-    return response.json();
-}
-    throw new Error('Network response was not ok.');
-})
-
-    .then(json => {console.log(json);
-        fetchCourses();
+function searchCourse() {
+    courseId=document.getElementById("courseId").value;
+    fetch("https://vvri.pythonanywhere.com/api/courses/" + courseId)
+        .then(response => response.json())
+        .then(data => {
+            if (data) {
+                console.log(data);
+                eredmeny.innerHTML+='Neve: '+data.name;
+            } else {
+                console.log("Course not found");
+            }
     })
-    .catch(error => console.error('There was a problem with the fetch operation:', error));
+.catch(error => console.log("Hiba történt: " + error))
+}
+
+function showStudents(){
+    dki.innerHTML="<th>Diák száma</th><th>Diák neve</th>";
+    fetch(surl)
+    .then(response => response.json())
+    .then(data => {
+        if (data)
+            data.forEach(element => {
+                console.log(element)
+                dki.innerHTML+='<tr class="students"><td>'+element.id+'</td><td>'+element.name+'</td></tr>';
+        })
+    })
+    .catch(error => console.log("Hiba történt: " + error))
+
 }
 
