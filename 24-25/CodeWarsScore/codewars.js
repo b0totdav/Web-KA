4011864f98640a4627ba0cd8db4b0c8d2a091be0
b0totdav <b0totdav@vasvari.org>
2024-09-19 14:42:16 +0200

    mondjuk, hogy kész

--- 24-25/CodeWarsScore/codewars.js
+++ 24-25/CodeWarsScore/codewars.js
@@ -1,18 +1,19 @@
 async function searchUser() {
     userName=document.getElementById("userName").value;
+    eredmeny.innerHTML="";
     try{
 
     const response=await fetch("https://www.codewars.com/api/v1/users/" + userName)
         const data=await response.json()
             if (data) {
                 console.log(data);
-                eredmeny.innerHTML+='<div class="user">Neve: '+data.name+'</div>'
-                eredmeny.innerHTML+='<div class="rank">Összesítve: Rangja: '+data.ranks.overall.name+'\n Színe: '+data.ranks.overall.color+'\n Pontszáma: '+data.ranks.overall.score+'</div>';
+                eredmeny.innerHTML+='<div class="user"><b>Neve:</b> '+data.name+'</div>'
+                eredmeny.innerHTML+='<div class="rank"><b>Összesítve:</b><br> <b>Rangja:</b> '+data.ranks.overall.name+'<br> <b>Színe:</b> '+data.ranks.overall.color+'<br> <b>Pontszáma:</b> '+data.ranks.overall.score+'</div>';
                 let uLanguages=data.ranks.languages;
-                eredmeny.innerHTML+='<div class="nyelvek"> <ol>';
+                eredmeny.innerHTML+='<div class="nyelvek"><b>Nyelv szerint:</b> <ol>';
                 for(let [key, value] of Object.entries(uLanguages))
                 {
-                    eredmeny.innerHTML+="<li>Nyelv: "+key+" Pontszám: "+value.score+"</li>";
+                    eredmeny.innerHTML+="<li><b>Nyelv:</b> "+key+"<br>&nbsp &nbsp; <b>Pontszám:</b> "+value.score+"</li>";
                 }
                 eredmeny.innerHTML+="</ol> </div>";
             }else {
