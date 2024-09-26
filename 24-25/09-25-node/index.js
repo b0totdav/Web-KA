5849fc9a9a155769fbc448915cfd011dfc1d7e01
b0totdav@vasvari.org <b0totdav@vasvari.org>
2024-09-26 12:29:25 +0200

    err

--- 24-25/09-25-node/index.js
+++ 24-25/09-25-node/index.js
@@ -6,6 +6,14 @@ const app = express();
 
 app.use(userRoutes)
 
+app.use((err,req,res,next) =>{
+    if(err){
+        console.log(`Error: ${err}`)
+        res.status(err.status || 500).send(`Hiba történt: ${err}`)
+    }
+    next()
+})
+
 app.listen(PORT,() =>{
 console.log(`Fut a szerver a localhost:${PORT} porton`)
 console.log('Press Ctrl+C to stop the server')})
--- 24-25/09-25-node/routes/user.js
+++ 24-25/09-25-node/routes/user.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url';
+import bodyParser from 'body-parser'
+import { appendFile } from 'fs';
 
 const router = express.Router();
 const __filename=fileURLToPath(import.meta.url)
 const __dirname=path.dirname(__filename)
+
+router.use(bodyParser.urlencoded({extended:false}))
 
 router.get('/',(req, res)=>
 {
@@ -15,9 +19,16 @@ router.get('/',(req, res)=>
 
 router.get('/users',(req, res)=>
     {
-        console.log("üzenet 2")
+        console.log("üzenet 2");
         res.sendFile(path.join(__dirname,'..','views','users.html'))
          
     })
+
+router.post('/create-user',(req,res) =>
+{
+    const userName= req.body.userName;
+    console.log('POST üzenet' + userName)
+    res.redirect("/users")
+})
 
 export default router
