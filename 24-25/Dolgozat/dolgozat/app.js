782eae08402d18c368b900753517c4c79bb1f9b2
b0totdav@vasvari.org <b0totdav@vasvari.org>
2025-01-08 15:10:08 +0100

    valami van

--- 24-25/Dolgozat/dolgozat/app.js
+++ 24-25/Dolgozat/dolgozat/app.js
@@ -1,9 +1,22 @@
 import express from 'express'
+import cors from 'cors'
+import router from './router/books.js'
+import { initializeDB } from "./database.js";
 
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
-  });
+app.use(cors());
+app.use(express.json());
+app.use("/api/books", router);
 
-app.listen(3000, 'localhost', () => { console.log(`Server is running on port http://localhost:3000`)})
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: err.message });
+});
+
+const startServer = async () => {
+    await initializeDB();
+    app.listen(3000, () => console.log("Server is running on port http://localhost:3000"));
+};
+
+startServer();
---
+++ 24-25/Dolgozat/dolgozat/database.js
@@ -0,0 +1,27 @@
+import sqlite3 from "sqlite3";
+
+const db = new sqlite3.Database("./database.sqlite");
+
+const initializeDB = async () => {
+    await dbRun("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, author TEXT, desc TEXT, year INTEGER)");
+};
+
+function dbQuery(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) reject(err);
+      else resolve(rows);
+    });
+  });
+}
+
+function dbRun(sql, params = []) {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, (err) => {
+      if (err) reject(err);
+      else resolve(this);
+    });
+  });
+}
+
+export { db, dbQuery, dbRun, initializeDB };
